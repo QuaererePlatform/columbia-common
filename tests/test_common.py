84f7cc4c1eca0c41a82c0b41c8c5baacb3217d55
ravenoak <ravenoak@virtualxistenz.com>
2019-08-07 16:45:26 -0700

    Adds initial tests

---
+++ tests/test_common.py
@@ -0,0 +1,6 @@
+from columbia_common.schemas import (CCDataSchemaV1, CCIndexesSchemaV1, CCScansSchemaV1)
+
+def test_common_crawl():
+    _ = CCDataSchemaV1()
+    _ = CCIndexesSchemaV1()
+    _ = CCScansSchemaV1()
